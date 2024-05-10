0e4b6bc9c6699499c3cd3f97f3f4bd583cff07f2
Lalomax <lalomax365@gmail.com>
2024-05-10 14:55:52 -0400

    step 1: countries

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,41 +1,103 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Notification from "./components/Notification";
 
 function App() {
   const [value, setValue] = useState("");
-  const [rates, setRates] = useState({});
-  const [currency, setCurrency] = useState(null);
+  const [countries, setCountries] = useState({});
+  const [country, setCountry] = useState(null);
+  const [found, setFound] = useState(null);
+  const [foundCountry, setFoundCountry] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  //recover all countries and set it to countries variable
 
   useEffect(() => {
-    console.log("effect run, currency is now", currency);
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setCountries(response.data.map((object) => object.name.common));
+        console.log("effect run, countries is filled", countries.length);
+        // console.log("orlando")
+      });
+  }, []);
 
-    // omitir si la moneda no está definida
-    if (currency) {
-      console.log("fetching exchange rates...");
-      axios
-        .get(`https://open.er-api.com/v6/latest/${currency}`)
-        .then((response) => {
-          setRates(response.data.rates);
-        });
+  useEffect(() => {
+    console.log("effect run, country is now", country);
+
+    // omitir si el pais no está definido
+    if (country) {
+      console.log("found", countriesToShow.length);
+      if (countriesToShow.length > 10) {
+        setMessage(`Too many matches, specify another filter`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 3000);
+
+        return;
+      }
+
+      if (countriesToShow.length === 1) {
+        console.log("trayendo el pais...", country);
+        axios
+          .get(
+            `https://studies.cs.helsinki.fi/restcountries/api/name/${country}`
+          )
+          .then((response) => {
+            setFoundCountry(response.data);
+            console.log("found", foundCountry);
+          });
+      }
+
+      if (countriesToShow.length <= 10) {
+        setFound(countriesToShow);
+        return;
+      }
     }
-  }, [currency]);
+  }, [country]);
+
+  // How many countries to show
+  const countriesToShow = !country
+    ? countries
+    : countries.filter((el) => {
+        if (el.toUpperCase().includes(country.toUpperCase())) return el;
+      });
 
   const handleChange = (event) => {
     setValue(event.target.value);
+    setFound(null);
+    setFoundCountry(null);
   };
 
   const onSearch = (event) => {
+    setCountry(value);
     event.preventDefault();
-    setCurrency(value);
   };
 
   return (
     <>
       <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type="submit">exchange rate</button>
+        find country: <input value={value} onChange={handleChange} />
+        <Notification message={message} />
+        {/* <button type="submit">exchange rate</button> */}
       </form>
-      <pre>{JSON.stringify(rates, null, 2)}</pre>
+      {found && found?.map((el) => <p key={el}>{el}</p>)}
+      {foundCountry && (
+        <div>
+          <h1>{foundCountry.common}</h1>
+          <p>{foundCountry.capital}</p>
+          <p>
+            {foundCountry && `area:`} {foundCountry.area}
+          </p>
+          {`Languages :`}
+          <ul>
+            {Object.values(foundCountry.languages).map((lan) => {
+              return <li>{lan}</li>;
+            })}
+          </ul>
+          <img src={foundCountry.flags.png} alt={foundCountry.flags.alt} />
+        </div>
+      )}
     </>
   );
 }
